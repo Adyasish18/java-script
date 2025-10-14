de01820443ff9be1061c8b690e07d4cd57c7ce0d
Adyasish18 <satpathyalok23@gmail.com>
2025-10-14 17:02:40 +0000

    container config done

--- basics/stack heap.js
+++
@@ -1,1 +0,0 @@
-// stack(primitive),heap(non-primitive)
--- basics/string.js
+++ basics/string.js
@@ -1,6 +1,20 @@
-const name="alok";
-console.log(`hi my name is ${name}`);
-const gamename=new String("Alok");
-console.log(gamename);
-// console.log(typeof gamename);
-console.log(gamename.__proto__);
+// const name="alok";
+// console.log(`hi my name is ${name}`);
+// const gamename=new String("Adyasishdotcom");
+// console.log(gamename);
+// // console.log(typeof gamename);
+// console.log(gamename.__proto__);
+// console.log(gamename.length);
+// console.log(gamename.charAt(2));
+// console.log(gamename.indexOf("l"));
+// const newstring=gamename.substring(0,2);
+// console.log(newstring);
+// const anotherstring=gamename.slice(-8,2);
+// console.log(anotherstring);
+const newstring="  happy  ";
+console.log(newstring.trim());
+const str="alok";
+console.log(str.replace('o','k'));
+// console.log(str.includes("hi"));This checks whether the string contains the substring "hi".
+
+// "alok" → does not contain "hi", so it returns:false
