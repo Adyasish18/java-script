30b8077df4977dfdd34adb43ce021cc2780a4730
Adyasish18 <satpathyalok23@gmail.com>
2025-10-15 15:54:27 +0000

    container config done

---
+++ basics/number-math.js
@@ -0,0 +1,37 @@
+// const score = 400
+// console.log(score.toFixed(1));
+
+
+//const balance = new Number(100);
+// when you use new Number(100), you are creating a Number object, not a primitive.
+// console.log(balance);
+
+// console.log(balance.toString().length);
+//console.log(balance.toFixed(1));//return type string
+// toFixed() formats a number to a fixed number of decimal places
+// and returns the result as a string.
+// number.toFixed(digits)
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+// toLocaleString() formats a number according to a locale (language-region).
+
+// 'en-IN' means English (India) → it uses Indian numbering system
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+// const min = 10
+// const max = 20
+
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min)
