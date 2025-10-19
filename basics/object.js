375167d69d6502765ebbb6d6ca3eb1b3e0b5f69b
Adyasish18 <satpathyalok23@gmail.com>
2025-10-19 08:24:30 +0000

    done

--- basics/object.js
+++ basics/object.js
@@ -16,23 +16,26 @@ const JsUser = {
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
+// an object is a collection of key-value pairs — where each key (also called a property name) is a string (or Symbol), and the value can be anything (string, number, array, function, or even another object).
 
-// console.log(JsUser.email)
+// console.log( JsUser.email)
 // console.log(JsUser["email"])
 // console.log(JsUser["full name"])
-// console.log(JsUser[mySym])
+// console.log( JsUser[mySym])
 
-JsUser.email = "hitesh@chatgpt.com"
-// Object.freeze(JsUser)
-JsUser.email = "hitesh@microsoft.com"
+// JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)//Makes an object immutable
+// JsUser.email = "hitesh@microsoft.com"
 // console.log(JsUser);
 
 JsUser.greeting = function(){
     console.log("Hello JS user");
 }
+
 JsUser.greetingTwo = function(){
     console.log(`Hello JS user, ${this.name}`);
 }
+console.log(JsUser);
 
 console.log(JsUser.greeting());
 console.log(JsUser.greetingTwo());
