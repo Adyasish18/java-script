19c536b350e5005bc3cd4c1dea952d8ef3960a35
Adyasish18 <satpathyalok23@gmail.com>
2025-10-20 07:20:43 +0000

    ok

--- basics/practice.js
+++ basics/practice.js
@@ -1,6 +1,12 @@
-// let a = 11;
-// let b = 8;
-let c = 0.997;
-console.log(c);
-console.log(c.toFixed(1));
-console.log(c.toPrecision(2));
+// // let a = 11;
+// // let b = 8;
+// let c = 0.997;
+// console.log(c);
+// console.log(c.toFixed(1));
+// console.log(c.toPrecision(2))
+function test() {
+     c = 30;
+}
+test();
+
+console.log(c); // ❌ Error: c is not defined
