861778ba26ba954e153ccd99e49a6de12755c563
Adyasish18 <satpathyalok23@gmail.com>
2025-10-31 13:27:56 +0000

    done

---
+++ basics/conditionalone.js
@@ -0,0 +1,56 @@
+// control structure
+// Control Flow in JavaScript refers to the order in which statements, instructions, or functions are executed in a program.
+// By default, JavaScript executes code from top to bottom, but control flow statements allow you to change the execution order based on conditions, loops, or function calls.
+// if
+// Executes a block only if a condition is true.
+const isUserloggedIn = true
+const temperature = 41
+
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+
+const balance = 1000
+
+if (balance > 500) console.log("test"),console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
