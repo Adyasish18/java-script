805aaefe84f09c13c3eb9bb2a44feec28035d5ed
Adyasish18 <satpathyalok23@gmail.com>
2025-10-31 17:24:29 +0000

    done

--- basics/iteration/one.js
+++ basics/iteration/one.js
@@ -1,35 +1,34 @@
 // for
-
-for (let i = 0; i <= 10; i++) {
-    const element = i;
-    if (element == 5) {
+// loop:A loop in JavaScript is a control structure that allows you to repeat a block of code multiple times — either a fixed number of times or until a certain condition is met.
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if (element == 5) {
         //console.log("5 is best number");
-    }
+    // }
     //console.log(element);
     
-}
+// }
 
 // console.log(element);
 
-for (let i = 1; i <= 10; i++) {
+// for (let i = 1; i <= 10; i++) {
     //console.log(`Outer loop value: ${i}`);
-   for (let j = 1; j <= 10; j++) {
+//    for (let j = 1; j <= 10; j++) {
     //console.log(`Inner loop value ${j} and inner loop ${i}`);
     //console.log(i + '*' + j + ' = ' + i*j );
-   }
+//    }
     
-}
-let myArray = ["flash", "batman", "superman"]
-//console.log(myArray.length);
-for (let index = 0; index < myArray.length; index++) {
-    const element = myArray[index];
-    //console.log(element);
-    
-}
+// }
+// let myArray = ["flash", "batman", "superman"]
+// console.log(myArray.length);
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+// }
 
 
 // break and continue
-
+// break:The break statement stops the loop immediately — no more iterations will run, even if the loop condition is still true.
 // for (let index = 1; index <= 20; index++) {
 //     if (index == 5) {
 //         console.log(`Detected 5`);
@@ -38,12 +37,11 @@ for (let index = 0; index < myArray.length; index++) {
 //    console.log(`Value of i is ${index}`);
     
 // }
-
-for (let index = 1; index <= 20; index++) {
-    if (index == 5) {
-        console.log(`Detected 5`);
-        continue
-    }
-   console.log(`Value of i is ${index}`);
-    
-}
+// continue:The continue statement skips the current iteration and moves to the next iteration of the loop.
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`Detected 5`);
+//         continue
+//     }
+//    console.log(`Value of i is ${index}`);
+// }
