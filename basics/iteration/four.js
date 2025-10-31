ff501bb1a19988a697e7c1f4d4742ba20160f608
Adyasish18 <satpathyalok23@gmail.com>
2025-10-31 18:01:14 +0000

    done

--- basics/iteration/four.js
+++ basics/iteration/four.js
@@ -1,26 +1,29 @@
-const myObject = {
-    js: 'javascript',
-    cpp: 'C++',
-    rb: "ruby",
-    swift: "swift by apple"
-}
+// for-in:The for...in loop is used to iterate (loop) over the keys (property names) of an object.
 
-for (const key in myObject) {
-    //console.log(`${key} shortcut is for ${myObject[key]}`);
-}
-
-const programming = ["js", "rb", "py", "java", "cpp"]
-
-for (const key in programming) {
-    //console.log(programming[key]);
-}
-
-// const map = new Map()
-// map.set('IN', "India")
-// map.set('USA', "United States of America")
-// map.set('Fr', "France")
-// map.set('IN', "India")
-
-// for (const key in map) {
-//     console.log(key);
+// It goes through each enumerable property in an object (and its prototype chain, unless filtered).
+// const myObject = {
+//     js: 'javascript',
+//     cpp: 'C++',
+//     rb: "ruby",
+//     swift: "swift by apple"
 // }
+
+// for (const key in myObject) {
+//     console.log(`${key} shortcut is for ${myObject[key]}`);
+// }
+
+// const programming = ["js", "rb", "py", "java", "cpp"]
+
+// for (const key in programming) {    
+//     console.log(programming[key]);
+// }
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+for (const key in map) {
+    console.log(key);
+}
