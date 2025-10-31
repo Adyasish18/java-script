ca0ef6de530b5ef5c0dd2e7eb55327de3c739ae6
Adyasish18 <satpathyalok23@gmail.com>
2025-10-31 13:39:14 +0000

    done

---
+++ basics/conditional2.js
@@ -0,0 +1,30 @@
+// The switch statement in JavaScript is used to perform different actions based on different conditions — similar to multiple if...else if statements, but cleaner and easier to read when you’re comparing one value against many possible matches.
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
