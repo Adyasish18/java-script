e15d113b868aeb3e6139d622c3fb5bff39385902
Adyasish Satpathy <satpathyalok23@gmail.com>
2026-01-27 18:39:24 +0000

    done

--- basics/conversion.js
+++ basics/conversion.js
@@ -12,3 +12,6 @@ console.log(1+2+"2");
 console.log("1"+2+2);
 console.log(2==="2");
 console.log(2=="2");
+// The reason is that an equality check == and comparisons > < >= <= work differently.
+// Comparisons convert null to a number, treating it as 0.
+// That’s why (3) null >= 0 is true and (1) null > 0 is false.
