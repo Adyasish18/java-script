bf3fde5ffc0060ec754933a3cc97fe58043937f4
Adyasish Satpathy <satpathyalok23@gmail.com>
2026-01-27 19:34:47 +0000

    done

--- basics/operator.js
+++ basics/operator.js
@@ -13,3 +13,17 @@
 
 // null == undefined // true
 // null === undefined // false
+
+
+// 2.🔹 What is the Ternary Operator?
+
+// The ternary operator is a short-hand replacement for if...else.
+
+// It’s the only operator in JavaScript that takes three operands.
+
+// condition ? expression_if_true : expression_if_false;
+
+// 3. What are operands?
+
+// Operands are the values or variables on which operators work.
+5+2 // 5,2 operands
