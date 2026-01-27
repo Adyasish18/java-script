853bfaa78d05bb75a07bcd381f1578871c5b22f1
Adyasish Satpathy <satpathyalok23@gmail.com>
2026-01-27 19:16:42 +0000

    done

---
+++ basics/operator.js
@@ -0,0 +1,15 @@
+// == vs ===
+
+// == is only compare values and Type conversion (coercion) happens automatically 
+
+
+
+// === is Compares value + type and no type conversion happens
+// 0 == false      // true
+// 0 === false     // false
+
+// "" == false     // true
+// "" === false    // false
+
+// null == undefined // true
+// null === undefined // false
